101e26761df0a8a77522d501e0b19a790910048b
ThiagoTheodoro <thiagot.deoliveira@gmail.com>
2021-10-29 16:12:25 -0300

    modify-js

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,5 +1,7 @@
 function welcome(){
-    alert("welcome to my first HTML page. I create that only for understand how github works.")
+    alert("welcome to my first HTML page. I create that only for understand how github works!!");
 
 
+}function JS(){
+    console.log("JS criado!");
 }
